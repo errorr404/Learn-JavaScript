749758a4bd5e0a1c20e109e666592a0318359a25
errorr404 <dkbishwash@gmail.com>
2018-05-18 22:42:28 +0530

    Calculate the age of a person by objects and constructors

--- Object Oriented JS/constructor_&_this_keyword.js
+++ Object Oriented JS/constructor_&_this_keyword.js
@@ -1,9 +1,32 @@
 // person constructor
-
-function Person(name) {
+/*
+function Person(name,age) {
   this.name = name; // here this keyword refrene the current object class
-  console.log(this);
+  this.age = age;
 }
 
-const dixit = new Person('dixit');
-const sawan = new Person('sawan');
+// console.log(this); //here this keyword is a window object
+
+const dixit = new Person('dixit',21);
+const sawan = new Person('sawan',20);
+
+console.log(sawan.age);
+*/
+
+// find the current age of a person
+
+function Person(name, dob ) {
+  this.name = name; 
+  this.birthday = new Date(dob);
+ // console.log(this.birthday);
+ this.calculateAge = function(){
+   const diff = Date.now() - this.birthday.getTime();
+   //console.log(diff);
+   const ageDate = new Date(diff);
+   //console.log(ageDate);
+   return  Math.abs(ageDate.getUTCFullYear()-1970);
+ }
+}
+
+const sawan = new Person('Dixit','11-26-1997');
+console.log(sawan.calculateAge());
