a34211fabfadc86b2048fd4cd4ec1eac16e3b3fb
errorr404 <dkbishwash@gmail.com>
2018-05-21 01:25:59 +0530

    callback function by asynchronous way

--- callback functions/app.js
+++ callback functions/app.js
@@ -4,9 +4,31 @@ const posts = [
 ];
 
 // function for createPost 
-function createPost(post){
+// function createPost(post){
+//   setTimeout(function() {
+//     posts.push(post);
+//   },2000);
+// }
+
+// function getPosts(){
+//   setTimeout(function(){
+//     let output = '';
+//     posts.forEach(function(post){
+//      output += `<li>${post.title}</li>`;
+     
+//     });
+//     document.body.innerHTML = output;
+//   },1000);
+// }
+
+// createPost({title: 'Post three',body: 'This is post three'});
+
+// getPosts();  
+
+function createPost(post,callback){
   setTimeout(function() {
     posts.push(post);
+    callback();
   },2000);
 }
 
@@ -18,9 +40,6 @@ function getPosts(){
      
     });
     document.body.innerHTML = output;
-  },2000);
+  },1000);
 }
-
-createPost({title: 'Post three',body: 'This is post three'});
-
-getPosts();
+createPost({title: 'Post three',body: 'This is post three'},getPosts);
